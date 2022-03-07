f51a469ca181fa79b474c8cb6bc4dfc9e1ae6c9e
1340691923@qq.com <1340691923@qq.com>
2022-03-07 14:17:13 +0800

    修改打包命令为正式服

--- build_manager_linux.bat
+++ build_manager_linux.bat
@@ -1,2 +1,2 @@
-cd vue && npm run build:stage && cd .. && set goos=linux&&go build  -ldflags="-w -s" -o bin/linux/manager cmd/manager/main.go
+cd vue && npm run build:prod && cd .. && set goos=linux&&go build  -ldflags="-w -s" -o bin/linux/manager cmd/manager/main.go
 echo "build success"
